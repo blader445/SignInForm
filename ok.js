ec5500c41d638918a83170e7a155668da80a8062
blader445 <65970821+blader445@users.noreply.github.com>
2020-10-13 18:54:13 +0200

    Add files via upload

---
+++ ok.js
@@ -0,0 +1,10 @@
+function getvalues(){
+
+    let a = document.getElementById("firstname").value;
+    let b = document.getElementById("lastname").value;
+    let c = document.getElementById("date").value;
+    let d = document.getElementById("email").value;
+    
+    console.log(a, b, c, d)
+
+}
